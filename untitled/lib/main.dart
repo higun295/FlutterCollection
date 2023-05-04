b6711782f7724d1ab561bb30abe589ee6447fa1b
higun295 <higun295@gmail.com>
2023-05-04 12:54:54 +0900

    20230504_1

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -11,121 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Home")),
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: Text(
-                "Nicholos - August 10th 2022",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(10,0,0,0),
-              child: Text(
-                "CURRENT WEIGHT",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                child: Text(
-                    "220 lb",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  )
-                )
-            ),
-            SizedBox(
-              height: 20,
-              width: double.infinity,
-            ),
-            Container(
-              height: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    child: Container(
-                      width: 160,
-                      height: 160,
-                      color: Colors.orange,
-                      child: Text(
-                        "Last 7 Days",
-                        style: TextStyle(
-                          fontSize: 25
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    child: Container(
-                      width: 160,
-                      height: 160,
-                      color: Colors.orange,
-                      child: Text(
-                        "Last 30 Days",
-                        style: TextStyle(
-                            fontSize: 25
-                        ),
-                      ),
-                    )
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    child: Container(
-                      width: 160,
-                      height: 160,
-                      color: Colors.orange,
-                      child: Text(
-                        "Last Year",
-                        style: TextStyle(
-                            fontSize: 25
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                      child: Container(
-                        width: 160,
-                        height: 160,
-                        color: Colors.orange,
-                        child: Text(
-                          "Median Weight",
-                          style: TextStyle(
-                              fontSize: 25
-                          ),
-                        ),
-                      )
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 200,
-              child: SizedBox(
-              ),
-            )
+            
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
