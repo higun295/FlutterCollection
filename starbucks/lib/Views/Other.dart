71b49cd439e9f57284ecda6474260351cb585371
higun295 <higun295@gmail.com>
2023-05-17 21:28:24 +0900

    20230517_1

--- starbucks/lib/Views/Other.dart
+++ starbucks/lib/Views/Other.dart
@@ -13,75 +13,58 @@ class Other extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            StarbucksSliverAppBar(
-              appBarTitle: appBarTitle,
-              appBarActions: [
-                IconButton(
-                  icon: SvgPicture.asset('images/bell.svg'),
-                  onPressed: () { },
+      body: CustomScrollView(
+        slivers: <Widget>[
+          StarbucksSliverAppBar(
+            appBarTitle: appBarTitle,
+            appBarActions: [
+              IconButton(
+                icon: SvgPicture.asset('images/bell.svg'),
+                onPressed: () { },
+              ),
+              IconButton(
+                icon: SvgPicture.asset('images/cog.svg'),
+                onPressed: () { },
+              )
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Table(
+              children: [
+                TableRow(
+                  children: [
+                    MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
+                    MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
+                  ],
                 ),
-                IconButton(
-                  icon: SvgPicture.asset('images/cog.svg'),
-                  onPressed: () { },
-                )
+                TableRow(
+                  children: [
+                    MenuButton(icon: 'images/bell.svg', label: '쿠폰 히스토리'),
+                    MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
+                  ],
+                ),
               ],
             ),
-            SliverToBoxAdapter(
-              child: Table(
-                children: [
-                  TableRow(
-                    children: [
-                      MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
-                      MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      MenuButton(icon: 'images/bell.svg', label: '쿠폰 히스토리'),
-                      MenuButton(icon: 'images/bell.svg', label: '쿠폰등록'),
-                    ],
-                  ),
-                ],
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16.0),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                childAspectRatio: 3.0,
               ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.all(16.0),
-              sliver: SliverGrid(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
-                  childAspectRatio: 3.0,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return MenuView();
-                  },
-                  childCount: 5
-                ),
-              )
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return MenuView();
+                },
+                childCount: 5
+              ),
             )
-            // SliverGrid(
-            //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //     crossAxisCount: 2,
-            //     mainAxisSpacing: 16,
-            //     crossAxisSpacing: 16,
-            //     childAspectRatio: 1.0
-            //   ),
-            //   delegate: SliverChildBuilderDelegate(
-            //     (BuildContext context, int index) {
-            //       return MenuButton(
-            //         icon: "images/bell.svg",
-            //         label: "Pay",
-            //       );
-            //     },
-            //     childCount: 15,
-            //   ),
-            // )
-          ],
-        )
+          )
+        ],
+      )
     );
   }
 }
