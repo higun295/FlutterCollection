ac041075ea8c79e51e4db8a8dd0bb87aa286c235
higun295 <higun295@gmail.com>
2023-05-19 22:21:49 +0900

    20230519_1

--- starbucks/lib/main.dart
+++ starbucks/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'MainPageTemplate.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
