8b1348f6cf928e8104775076d6336d17e5fd7d2c
HEEKWON <higun295@gmail.com>
2023-11-28 09:47:38 +0900

    20231128_2

--- Challenge/lib/main.dart
+++ Challenge/lib/main.dart
@@ -1,58 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'FlutterBoot',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('sdfsdf'),
-            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-            elevation: 0,
-          ),
-          body: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'FlutterBoot Plus',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40
+          appBar: AppBar(),
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'FlutterBoot Plus',
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Spacer(flex: 1),
-                Column(
-                  children: [
-                    CustomRow(
-                        icon: '전기',
-                        title: 'Premium features',
-                        content: 'Plus subscribers have access to FlutterBoot+ and out latest beta features.'),
-                    CustomRow(
-                        icon: '불',
-                        title: 'Priority access',
-                        content: 'You\'ll be able to use FlutterBoot+ even when demand is high'),
-                    CustomRow(
-                        icon: '계기',
-                        title: 'Ultra-fase',
-                        content: 'Enjoy even faster response speeds when using FlutterBoot'),
-                  ],
-                ),
-                Spacer(flex: 25),
-                Text('Restore subscription'),
-                Spacer(flex: 1),
+                  const SizedBox(height: 24),
+                  // Column(
+                  //   children: [
+                  //      CustomRow(
+                  //         icon: '전기',
+                  //         title: 'Premium features',
+                  //         content: 'Plus subscribers have access to FlutterBoot+ and out latest beta features.'),
+                  //     CustomRow(
+                  //         icon: '불',
+                  //         title: 'Priority access',
+                  //         content: 'You\'ll be able to use FlutterBoot+ even when demand is high'),
+                  //     CustomRow(
+                  //         icon: '계기',
+                  //         title: 'Ultra-fase',
+                  //         content: 'Enjoy even faster response speeds when using FlutterBoot'),
+                  //   ],
+                  // ),
+                  Spacer(flex: 25),
+                  Text('Restore subscription'),
+                  Spacer(flex: 1),
 
-                Column(
-                  children: [
-                    const Text('Auto-renews for \$25/month until canceled'),
-                    const Text('버튼영역')
-                  ],
-                )
-              ]
+                  Column(
+                    children: [
+                      const Text('Auto-renews for \$25/month until canceled'),
+                      const Text('버튼영역')
+                    ],
+                  )
+                ]
+            )
           )
       ),
     );
@@ -85,7 +91,7 @@ class CustomRow extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Flexible(child: Text(content))
+            Text(content)
           ],
         )
       ],
