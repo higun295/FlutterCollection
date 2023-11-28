b5f32eb1c75bc0ba061914bbf90bc54fd6ced5c3
HEEKWON <higun295@gmail.com>
2023-11-28 10:33:52 +0900

    20231128_3

--- Challenge/lib/main.dart
+++ Challenge/lib/main.dart
@@ -7,6 +7,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Widget _buildAdCard({
+    required IconData iconData,
+    required String title,
+    required String content
+  }){
+   return Row(
+     crossAxisAlignment: CrossAxisAlignment.start,
+     children: [
+       Icon(
+         iconData,
+         size: 36,
+       ),
+       const SizedBox(width: 24),
+       Expanded(
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           mainAxisSize: MainAxisSize.min,
+           children: [
+             Text(
+               title,
+               style: const TextStyle(
+                 fontSize: 16,
+                 fontWeight: FontWeight.w600,
+               ),
+             ),
+             const SizedBox(height: 2),
+             Text(
+                 content
+             )
+           ],
+         )
+       )
+     ],
+   );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,36 +63,33 @@ class MyApp extends StatelessWidget {
                     'FlutterBoot Plus',
                     style: TextStyle(
                       fontSize: 36,
-                      fontWeight: FontWeight.bold,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
                   const SizedBox(height: 24),
+                  _buildAdCard(
+                      iconData: Icons.bolt,
+                      title: 'Premium features',
+                      content: 'Plus subscribers have access to FlutterBoot+ and out latest beta features.'
+                  ),
+                  const SizedBox(height: 24),
+                  _buildAdCard(
+                      iconData: Icons.whatshot,
+                      title: 'Priority access',
+                      content: 'You\'ll be able to use FlutterBoot+ even when demand is high',
+                  ),
+                  const SizedBox(height: 24),
+                  _buildAdCard(
+                    iconData: Icons.speed,
+                    title: 'Ultra-fase',
+                    content: 'Enjoy even faster response speeds when using FlutterBoot',
+                  ),
                   // Column(
                   //   children: [
-                  //      CustomRow(
-                  //         icon: '전기',
-                  //         title: 'Premium features',
-                  //         content: 'Plus subscribers have access to FlutterBoot+ and out latest beta features.'),
-                  //     CustomRow(
-                  //         icon: '불',
-                  //         title: 'Priority access',
-                  //         content: 'You\'ll be able to use FlutterBoot+ even when demand is high'),
-                  //     CustomRow(
-                  //         icon: '계기',
-                  //         title: 'Ultra-fase',
-                  //         content: 'Enjoy even faster response speeds when using FlutterBoot'),
+                  //     const Text('Auto-renews for \$25/month until canceled'),
+                  //     const Text('버튼영역')
                   //   ],
-                  // ),
-                  Spacer(flex: 25),
-                  Text('Restore subscription'),
-                  Spacer(flex: 1),
-
-                  Column(
-                    children: [
-                      const Text('Auto-renews for \$25/month until canceled'),
-                      const Text('버튼영역')
-                    ],
-                  )
+                  // )
                 ]
             )
           )
