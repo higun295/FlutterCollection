0701bf819dbbb0d3c8c47c9c6e669d6e4a1e3b68
HEEKWON <higun295@gmail.com>
2023-11-30 16:32:30 +0900

    working challenge4

--- Challenge4/lib/main.dart
+++ Challenge4/lib/main.dart
@@ -11,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello TextField!'),
     );
   }
 }
@@ -29,38 +31,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            GestureDetector(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.45,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                  ),
+                )
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.05,
             ),
+            GestureDetector(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.45,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                  ),
+                )
+              )
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
