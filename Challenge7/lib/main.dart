bd276eda0ae43e36e89ef38f3249f6612d1e2b5e
HEEKWON <higun295@gmail.com>
2023-12-05 14:31:58 +0900

    doing Challenge7

--- Challenge7/lib/main.dart
+++ Challenge7/lib/main.dart
@@ -10,11 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello Overlay'),
     );
   }
 }
@@ -41,27 +43,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(
+          child: Text(
+            widget.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400
+            ),
+          ),
+        )
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 30),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: OutlinedButton(onPressed: null, child: Text('Hello!')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 5),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: OutlinedButton(onPressed: null, child: Text('Hello!')),
+            ),
+            SizedBox(height: 5),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: OutlinedButton(onPressed: null, child: Text('Hello!')),
+            ),
+            SizedBox(height: 5),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: OutlinedButton(onPressed: null, child: Text('Hello!')),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
